81b25b4c7349911765cfb47cadad0b89557820ed
Thomas Merlaud <thomas36.merlaud@gmail.com>
2024-02-22 11:40:32 +0100

    upload images, abandon amazon

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:titre, :description, images: [])
+      params.require(:post).permit(:titre, :description, :image)
     end
 end
